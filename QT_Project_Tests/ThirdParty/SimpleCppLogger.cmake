6ae09f8126d5cd6f261cda35995216d95049e3ab
Adrian Helbig <adrian_helbig@outlook.de>
2026-05-09 23:14:08 +0200

    build(cmake): enhance dependency management and organize utility targets
    - Create `CMake/IncludeThirdParty.cmake` defining `include_third_party_library` for standardized dependency handling (download, build, link).
    - Add `${MAIN_PROJECT_NAME}_INCLUDE_THIRD_LIBS_INTO_SOLUTION` option to optionally include dependency sources in the IDE solution.
    - Refactor `SimpleCppLogger.cmake` across main and test projects to use the new macro, eliminating duplicated integration boilerplate.
    - Namespace custom targets (translations, qrc) with `${PROJECT_NAME}_` to prevent global collisions.
    - Group utility targets into a `_Tasks` IDE folder to reduce root-level clutter.
    - Bump minimum CMake version to 3.29.3 and set default `CMAKE_BUILD_TYPE` to "Debug".

--- QT_Project_Tests/ThirdParty/SimpleCppLogger.cmake
+++ QT_Project_Tests/ThirdParty/SimpleCppLogger.cmake
@@ -1,46 +1,8 @@
 # SimpleCppLogger Integration
-set(THIRD_PARTY_TARGET "SimpleCppLogger")
-set(GIT_TAG "main")
-set(PROJECT_DIR_NAME "${THIRD_PARTY_TARGET}_${GIT_TAG}")
-set(THIRD_PARTY_TARGET_DIR "${THIRD_PARTY_INCLUDE_DIR}/${PROJECT_DIR_NAME}")
-set(CMAKE_ARGS "-D ${THIRD_PARTY_TARGET}_BUILD_TARGET_TYPE:STRING=static_library -D MAIN_PROJECT_NAME:STRING=SimpleCppLogger")
-set(SimpleCppLogger_INSTALL_ROOT "${THIRD_PARTY_TARGET_DIR}/${THIRD_PARTY_TARGET}_install")
-set(SimpleCppLogger_INCLUDE_DIR ${SimpleCppLogger_INSTALL_ROOT}/${CMAKE_BUILD_TYPE}/include)
-set(SimpleCppLogger_LIBRARY ${SimpleCppLogger_INSTALL_ROOT}/${CMAKE_BUILD_TYPE}/lib/SimpleCppLogger.lib)
-set(SimpleCppLogger_DIR "")
 
-# Dynamically find all installed third-party packages to help find_dependency()
-# matching the pattern: ThirdParty/<Project>_<Tag>/<Target>_install/<Config>
-file(GLOB _installed_third_parties
-    "${THIRD_PARTY_INCLUDE_DIR}/*/*_install/${CMAKE_BUILD_TYPE}"
-    "${THIRD_PARTY_INCLUDE_DIR}/*/*_install"
+include_third_party_library(
+    "SimpleCppLogger"
+    "https://github.com/Dingola/SimpleCppLogger.git"
+    "main"
+    "PUBLIC"
 )
-list(APPEND CMAKE_PREFIX_PATH ${_installed_third_parties} "${SimpleCppLogger_INSTALL_ROOT}/${CMAKE_BUILD_TYPE}")
-
-find_package(SimpleCppLogger HINTS ${SimpleCppLogger_INSTALL_ROOT}/${CMAKE_BUILD_TYPE}/lib/cmake/SimpleCppLogger NO_DEFAULT_PATHS)
-
-if(SimpleCppLogger_FOUND)
-    message("SimpleCppLogger found")
-else()
-    message("SimpleCppLogger not found. Downloading and invoking cmake ..")
-    build_third_party_project(
-        false
-        ${THIRD_PARTY_TARGET}
-        https://github.com/Dingola/SimpleCppLogger.git
-        ${GIT_TAG}
-        ${THIRD_PARTY_TARGET_DIR}
-        ${CMAKE_BUILD_TYPE}
-		${CMAKE_ARGS}
-    )
-	
-    # Re-scan third-party installs after the build to pick up dependencies (CommonLib) that were just installed
-    file(GLOB _installed_third_parties
-        "${THIRD_PARTY_INCLUDE_DIR}/*/*_install/${CMAKE_BUILD_TYPE}"
-        "${THIRD_PARTY_INCLUDE_DIR}/*/*_install"
-    )
-    list(APPEND CMAKE_PREFIX_PATH ${_installed_third_parties} "${SimpleCppLogger_INSTALL_ROOT}/${CMAKE_BUILD_TYPE}")
-
-	find_package(SimpleCppLogger REQUIRED HINTS ${SimpleCppLogger_INSTALL_ROOT}/${CMAKE_BUILD_TYPE}/lib/cmake/SimpleCppLogger NO_DEFAULT_PATHS)
-endif()
-
-target_link_libraries(${PROJECT_NAME} PUBLIC SimpleCppLogger)
